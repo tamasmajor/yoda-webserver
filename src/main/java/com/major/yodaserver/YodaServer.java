a9f9c5a97320d8b10064757c4d85ca08704827d7
tamasmajor <majortamas8@gmail.com>
2019-09-18 20:10:50 +0200

    implemented the loop for accepting the requests

--- src/main/java/com/major/yodaserver/YodaServer.java
+++ src/main/java/com/major/yodaserver/YodaServer.java
@@ -1,7 +1,15 @@
 package com.major.yodaserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import javax.net.ServerSocketFactory;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.major.yodaserver.interrupter.ServerInterrupter;
+import com.major.yodaserver.requestprocessor.factory.RequestProcessorFactory;
 
 public class YodaServer {
     private static final Logger logger = LoggerFactory.getLogger(YodaServer.class);
@@ -10,7 +18,9 @@ public class YodaServer {
     protected static final int LOWEST_AVAILABLE_PORT = 1;
     protected static final int HIGHEST_AVAILABLE_PORT = 65535;
 
-    private final ServerSettings serverSettings;
+    private final RequestProcessorFactory requestProcessorFactory;
+    private final ServerSocketFactory serverSocketFactory;
+    private final ServerInterrupter interrupter;
     private final int port;
 
     public YodaServer(ServerSettings serverSettings) {
@@ -19,12 +29,22 @@ public class YodaServer {
 
     public YodaServer(ServerSettings serverSettings, int port) {
         validatePort(port);
-        this.serverSettings = serverSettings;
+        this.requestProcessorFactory = serverSettings.getRequestProcessorFactory();
+        this.serverSocketFactory = serverSettings.getServerSocketFactory();
+        this.interrupter = serverSettings.getServerInterrupter();
         this.port = port;
     }
 
     public void listen() {
         logger.info("Running on port: {}", port);
+        try (ServerSocket server = serverSocketFactory.createServerSocket(port)) {
+            while (!interrupter.actived()) {
+                server.accept();
+            }
+        } catch (IOException e) {
+            logger.error("Could not create the server socket on port " + port, e);
+            throw new RuntimeException("Server startup failure");
+        }
     }
 
     private void validatePort(int port) {
--- src/test/java/com/major/yodaserver/YodaServerTest.java
+++ src/test/java/com/major/yodaserver/YodaServerTest.java
@@ -1,6 +1,8 @@
 package com.major.yodaserver;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import javax.net.ServerSocketFactory;
 
 import org.junit.Before;
@@ -15,7 +17,7 @@ import com.major.yodaserver.interrupter.ServerInterrupter;
 import com.major.yodaserver.requestprocessor.factory.RequestProcessorFactory;
 
 import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class YodaServerTest {
@@ -35,8 +37,11 @@ public class YodaServerTest {
     @Mock
     private ServerInterrupter interrupter;
 
+    private YodaServer yodaServer;
+
     @Before
     public void setUp() throws Exception {
+        yodaServer = mockedYoda();
         when(serverSocketFactory.createServerSocket(anyInt())).thenReturn(serverSocket);
     }
 
@@ -54,6 +59,45 @@ public class YodaServerTest {
         thrown.expect(IllegalArgumentException.class);
         thrown.expectMessage("Port '" + testedPort + "' is not valid");
         new YodaServer(new ServerSettings(serverSocketFactory, requestProcessorFactory, interrupter), testedPort);
+    }
+
+    @Test
+    public void listen_interruptedBeforeFirstRequest_doesNotAcceptAnyRequestBeforeShutdown() throws IOException {
+        // given
+        when(interrupter.actived()).thenReturn(true);
+        // when
+        yodaServer.listen();
+        // then
+        verify(interrupter, times(1)).actived();
+        verify(serverSocket, times(0)).accept();
+    }
+
+    @Test
+    public void listen_interruptedOnlyAfterFirstRequest_callsAcceptOneRequestBeforeShutdown() throws IOException {
+        // given
+        when(interrupter.actived()).thenReturn(false, true);
+        when(serverSocket.accept()).thenReturn(new Socket());
+        // when
+        yodaServer.listen();
+        // then
+        verify(interrupter, times(2)).actived();
+        verify(serverSocket, times(1)).accept();
+    }
+    
+    @Test
+    public void listen_couldNotBoundServerSocketToPort_throwsRuntimeException() throws IOException {
+        // expected exception
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Server startup failure");
+        // given
+        when(serverSocketFactory.createServerSocket(anyInt())).thenThrow(new IOException("Something wrong happened"));
+        // when
+        yodaServer.listen();
+        // then - throws expected exception
+    }
+
+    private YodaServer mockedYoda() {
+        return new YodaServer(new ServerSettings(serverSocketFactory, requestProcessorFactory, interrupter));
     }
 
 }

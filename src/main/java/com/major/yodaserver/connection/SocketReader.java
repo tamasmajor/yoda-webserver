2305194db2f789251d39c8fc087f71d3bd2c9ca7
tamasmajor <majortamas8@gmail.com>
2019-09-28 20:37:32 +0200

    implemented the reading of the request-line from the HTTP request

--- src/main/java/com/major/yodaserver/connection/SocketReader.java
+++ src/main/java/com/major/yodaserver/connection/SocketReader.java
@@ -10,20 +10,62 @@ public class SocketReader {
     private static final String CHARSET = "ASCII";
     private final BufferedReader reader;
 
+    private boolean requestLineProcessed;
+    private RequestLine requestLine;
+
     public SocketReader(Socket connection) throws IOException {
         reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(connection.getInputStream()), CHARSET));
     }
 
-    public String getMethod() throws IOException {
-        return reader.readLine().split("\\s+")[0];
+    public String getRequestMethod() throws IOException {
+        return obtainRequestLine().getMethod();
     }
 
-
     public String getRequestUri() throws IOException {
-        return reader.readLine().split("\\s+")[1];
+        return obtainRequestLine().getRequestUri();
     }
 
     public String getHttpVersion() throws IOException {
-        return reader.readLine().split("\\s+")[2];
+        return obtainRequestLine().getHttpVersion();
+    }
+
+    private RequestLine obtainRequestLine() throws IOException {
+        if (!requestLineProcessed) {
+            requestLine = RequestLine.from(reader.readLine());
+            requestLineProcessed = true;
+        }
+        return requestLine;
+    }
+
+    private static class RequestLine {
+        private final String method;
+        private final String requestUri;
+        private final String httpVersion;
+
+        private RequestLine(String method, String requestUri, String httpVersion) {
+            this.method = method;
+            this.requestUri = requestUri;
+            this.httpVersion = httpVersion;
+        }
+        
+        public static RequestLine from(String requestLine) {
+            String[] token = requestLine.split("\\s+");
+            if (token.length != 3) {
+                throw new IllegalArgumentException("Request-Line contains invalid number of tokens");
+            }
+            return new RequestLine(token[0], token[1], token[2]);
+        }
+
+        public String getMethod() {
+            return method;
+        }
+
+        public String getRequestUri() {
+            return requestUri;
+        }
+
+        public String getHttpVersion() {
+            return httpVersion;
+        }
     }
 }
--- src/test/java/com/major/yodaserver/connection/SocketReaderTest.java
+++ src/test/java/com/major/yodaserver/connection/SocketReaderTest.java
@@ -2,7 +2,9 @@ package com.major.yodaserver.connection;
 
 import java.io.IOException;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.major.yodaserver.helper.VerifierSocket;
 
@@ -10,26 +12,29 @@ import static org.junit.Assert.assertEquals;
 
 public class SocketReaderTest {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
-    public void getMethod_requestLineWitGet_returnsGet() throws IOException {
+    public void getRequestMethod_requestLineWitGet_returnsGet() throws IOException {
         // given
         VerifierSocket socket = new VerifierSocket();
         socket.addRequestLine("GET /hello.png HTTP/1.1");
         // when
         SocketReader socketReader = new SocketReader(socket);
         // then
-        assertEquals("GET", socketReader.getMethod());
+        assertEquals("GET", socketReader.getRequestMethod());
     }
 
     @Test
-    public void getMethod_requestLineWithPost_returnsPost() throws IOException {
+    public void getRequestMethod_requestLineWithPost_returnsPost() throws IOException {
         // given
         VerifierSocket socket = new VerifierSocket();
         socket.addRequestLine("POST /hello.png HTTP/1.1");
         // when
         SocketReader socketReader = new SocketReader(socket);
         // then
-        assertEquals("POST", socketReader.getMethod());
+        assertEquals("POST", socketReader.getRequestMethod());
     }
 
     @Test
@@ -63,5 +68,32 @@ public class SocketReaderTest {
         SocketReader socketReader = new SocketReader(socket);
         // then
         assertEquals("HTTP/1.1", socketReader.getHttpVersion());
+    }
+    
+    @Test
+    public void getRequestLineInformationMultipleTimes_returnsRequestedInformation() throws IOException {
+        // given
+        VerifierSocket socket = new VerifierSocket();
+        socket.addRequestLine("GET /hello.png HTTP/1.1");
+        // when
+        SocketReader socketReader = new SocketReader(socket);
+        // then
+        assertEquals("/hello.png", socketReader.getRequestUri());
+        assertEquals("/hello.png", socketReader.getRequestUri());
+        assertEquals("/hello.png", socketReader.getRequestUri());
+    }
+    
+    @Test
+    public void requestLineHasInvalidNumberOfTokens_thrownException() throws IOException {
+        // expected exception
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Request-Line contains invalid number of tokens");
+        // given
+        VerifierSocket socket = new VerifierSocket();
+        socket.addRequestLine("GET /hello.png");
+        // when
+        SocketReader socketReader = new SocketReader(socket);
+        socketReader.getRequestMethod();
+        // throws expected exception
     }
 }

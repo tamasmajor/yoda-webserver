00b6ee041aad87c78db4a03ad1236da5f8c5f74c
tamasmajor <majortamas8@gmail.com>
2019-10-20 13:09:01 +0200

    added the explorer skeleton which will generate the HTML for the directory requests

---
+++ src/main/java/com/major/yodaserver/requestprocessor/plugin/DirectoryExplorer.java
@@ -0,0 +1,7 @@
+package com.major.yodaserver.requestprocessor.plugin;
+
+import java.io.File;
+
+public interface DirectoryExplorer {
+    String renderPage(File root);
+}
---
+++ src/main/java/com/major/yodaserver/requestprocessor/plugin/SimpleDirectoryExplorer.java
@@ -0,0 +1,12 @@
+package com.major.yodaserver.requestprocessor.plugin;
+
+import java.io.File;
+
+public class SimpleDirectoryExplorer implements DirectoryExplorer {
+
+    @Override
+    public String renderPage(File root) {
+        return null;
+    }
+
+}

e248ad7b9cbe1c234594d794c12e6c7e63728b9c
tamasmajor <majortamas8@gmail.com>
2019-10-23 00:04:26 +0200

    do not allow the user to request outside of the root directory, also show 404 when the resource is not found

--- src/main/java/com/major/yodaserver/requestprocessor/YodaRequestProcessor.java
+++ src/main/java/com/major/yodaserver/requestprocessor/YodaRequestProcessor.java
@@ -15,8 +15,10 @@ import com.major.yodaserver.requestprocessor.plugin.DirectoryExplorer;
 
 public class YodaRequestProcessor extends RequestProcessor {
     private static final Logger logger = LoggerFactory.getLogger(YodaRequestProcessor.class);
-    private static final String NOT_SUPPORTED_MESSAGE_BODY = "<HTML><HEAD><TITLE>Not yet supported</TITLE></HEAD>" +
-                                                             "<BODY>501 - Not yet supported</BODY></HTML>";
+    protected static final String NOT_SUPPORTED_MESSAGE_BODY = "<HTML><HEAD><TITLE>Not yet supported</TITLE></HEAD>" +
+                                                               "<BODY>501 - Not yet supported</BODY></HTML>";
+    protected static final String NOT_FOUND_MESSAGE_BODY = "<HTML><HEAD><TITLE>Not found</TITLE></HEAD>" +
+                                                           "<BODY>404 - File not found</BODY></HTML>";
 
     private final DirectoryExplorer directoryExplorer;
 
@@ -38,27 +40,31 @@ public class YodaRequestProcessor extends RequestProcessor {
             if (requestMethod.equals("GET")) {
                 String uri = socketReader.getRequestUri();
                 String contentType = URLConnection.getFileNameMap().getContentTypeFor(uri);
-                // TODO: make sure clients cannot navigate outside of the document root with ".."
                 File requestedResource = new File(rootDir, uri.substring(1, uri.length()));
 
-                if (requestedResource.isDirectory()) {
-                    String data = directoryExplorer.renderPage(rootDir, requestedResource);
-                    response.write(asResponseLine("HTTP/1.1 200 OK"));
-                    response.write(asResponseLine("Server: YodaServer 0.0.1"));
-                    response.write(asResponseLine("Content-Length: " + data.length()));
-                    response.write(asResponseLine("Content-Type: text/html"));
-                    response.write(asResponseLine(""));
-                    response.write(data.getBytes());
+                if (requestedResourceAccessible(requestedResource, rootDir)) {
+                    if (requestedResource.isDirectory()) {
+                        String data = directoryExplorer.renderPage(rootDir, requestedResource);
+                        response.write(asResponseLine("HTTP/1.1 200 OK"));
+                        response.write(asResponseLine("Server: YodaServer 0.0.1"));
+                        response.write(asResponseLine("Content-Length: " + data.length()));
+                        response.write(asResponseLine("Content-Type: text/html"));
+                        response.write(asResponseLine(""));
+                        response.write(data.getBytes());
+                    } else {
+                        byte[] resource = Files.readAllBytes(requestedResource.toPath());
+                        response.write(asResponseLine("HTTP/1.1 200 OK"));
+                        response.write(asResponseLine("Server: YodaServer 0.0.1"));
+                        response.write(asResponseLine("Content-Length: " + resource.length));
+                        response.write(asResponseLine("Content-Type: " + contentType));
+                        response.write(asResponseLine(""));
+                        response.write(resource);
+                    }
+                    response.flush();
                 } else {
-                    byte[] resource = Files.readAllBytes(requestedResource.toPath());
-                    response.write(asResponseLine("HTTP/1.1 200 OK"));
-                    response.write(asResponseLine("Server: YodaServer 0.0.1"));
-                    response.write(asResponseLine("Content-Length: " + resource.length));
-                    response.write(asResponseLine("Content-Type: " + contentType));
-                    response.write(asResponseLine(""));
-                    response.write(resource);
+                    response.write(constructFileNotFoundResponse().getBytes());
+                    response.flush();
                 }
-                response.flush();
             } else {
                 response.write(constructNotSupportedResponse().getBytes());
                 response.flush();
@@ -74,6 +80,10 @@ public class YodaRequestProcessor extends RequestProcessor {
         }
     }
 
+    private boolean requestedResourceAccessible(File requestedResource, File rootDir) throws IOException {
+        return requestedResource.canRead() && requestedResource.getCanonicalPath().startsWith(rootDir.getCanonicalPath());
+    }
+
     private String constructNotSupportedResponse() {
         return String.join(RESPONSE_LINE_TERMINATOR, "HTTP/1.1 501 Not Implemented",
                                                      "Server: YodaServer 0.0.1",
@@ -81,6 +91,15 @@ public class YodaRequestProcessor extends RequestProcessor {
                                                      "Content-type: text/html",
                                                      "",
                                                      NOT_SUPPORTED_MESSAGE_BODY);
+    }
+
+    private String constructFileNotFoundResponse() {
+        return String.join(RESPONSE_LINE_TERMINATOR, "HTTP/1.1 404 File Not Found",
+                                                     "Server: YodaServer 0.0.1",
+                                                     "Content-Length: " + NOT_FOUND_MESSAGE_BODY.getBytes().length,
+                                                     "Content-type: text/html",
+                                                     "",
+                                                     NOT_FOUND_MESSAGE_BODY);
     }
 
 
--- src/test/java/com/major/yodaserver/requestprocessor/YodaRequestProcessorTest.java
+++ src/test/java/com/major/yodaserver/requestprocessor/YodaRequestProcessorTest.java
@@ -7,23 +7,74 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import com.major.yodaserver.helper.VerifierSocket;
 
+import static com.major.yodaserver.requestprocessor.YodaRequestProcessor.NOT_FOUND_MESSAGE_BODY;
+import static com.major.yodaserver.requestprocessor.YodaRequestProcessor.NOT_SUPPORTED_MESSAGE_BODY;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class YodaRequestProcessorTest {
-    private static final String TEST_RESOURCES = "src/test/resources";
 
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    @Test
+    public void requestedResourceDoesNotExist_notFoundReturned() {
+        // given
+        VerifierSocket verifierSocket = new VerifierSocket();
+        verifierSocket.addRequestLine("GET /does-not-exists HTTP/1.1");
+        YodaRequestProcessor requestProcessor = new YodaRequestProcessor(tempFolder.getRoot(), verifierSocket, null);
+        // when
+        requestProcessor.run();
+        // then
+        List<String> responseHeaderLines = verifierSocket.getHeaderLines();
+        assertEquals(5, responseHeaderLines.size());
+        Iterator<String> itHeaderLines = responseHeaderLines.iterator();
+        assertEquals("HTTP/1.1 404 File Not Found", itHeaderLines.next());
+        assertEquals("Server: YodaServer 0.0.1", itHeaderLines.next());
+        assertEquals("Content-Length: " + NOT_FOUND_MESSAGE_BODY.getBytes().length, itHeaderLines.next());
+        assertEquals("Content-type: text/html", itHeaderLines.next());
+        assertEquals("", itHeaderLines.next());
+        assertEquals(NOT_FOUND_MESSAGE_BODY, verifierSocket.bodyAsString());
+    }
+
+    @Test
+    public void requestedResourceExistButAboveRootInHierarcy_notFoundReturned() throws IOException {
+        // given
+        tempFolder.newFolder("above-root-2", "root");
+        File root = new File(tempFolder.getRoot() + "/above-root-2/root");
+        VerifierSocket verifierSocket = new VerifierSocket();
+        verifierSocket.addRequestLine("GET /../ HTTP/1.1");
+        YodaRequestProcessor requestProcessor = new YodaRequestProcessor(root, verifierSocket, null);
+        // when
+        requestProcessor.run();
+        // then
+        List<String> responseHeaderLines = verifierSocket.getHeaderLines();
+        assertEquals(5, responseHeaderLines.size());
+        Iterator<String> itHeaderLines = responseHeaderLines.iterator();
+        assertEquals("HTTP/1.1 404 File Not Found", itHeaderLines.next());
+        assertEquals("Server: YodaServer 0.0.1", itHeaderLines.next());
+        assertEquals("Content-Length: " + NOT_FOUND_MESSAGE_BODY.getBytes().length, itHeaderLines.next());
+        assertEquals("Content-type: text/html", itHeaderLines.next());
+        assertEquals("", itHeaderLines.next());
+        assertEquals(NOT_FOUND_MESSAGE_BODY, verifierSocket.bodyAsString());
+    }
+    
     @Test
     public void imageResourceRequested_imageResourceReturned() throws IOException {
         // given
         byte[] expectedFileContent = Files.readAllBytes(Paths.get("src", "test", "resources", "test", "image01.png"));
+        File testFile = new File(tempFolder.newFolder("test").getPath() + "/image01.png");
+        testFile.createNewFile();
+        Files.write(testFile.toPath(), expectedFileContent);
         VerifierSocket verifierSocket = new VerifierSocket();
         verifierSocket.addRequestLine("GET /test/image01.png HTTP/1.1");
-        YodaRequestProcessor requestProcessor = new YodaRequestProcessor(new File(TEST_RESOURCES), verifierSocket, null);
+        YodaRequestProcessor requestProcessor = new YodaRequestProcessor(tempFolder.getRoot(), verifierSocket, null);
         // when
         requestProcessor.run();
         // then
@@ -52,11 +103,10 @@ public class YodaRequestProcessorTest {
         Iterator<String> itHeaderLines = responseHeaderLines.iterator();
         assertEquals("HTTP/1.1 501 Not Implemented", itHeaderLines.next());
         assertEquals("Server: YodaServer 0.0.1", itHeaderLines.next());
-        assertEquals("Content-Length: 94", itHeaderLines.next());
+        assertEquals("Content-Length: " + NOT_SUPPORTED_MESSAGE_BODY.getBytes().length, itHeaderLines.next());
         assertEquals("Content-type: text/html", itHeaderLines.next());
         assertEquals("", itHeaderLines.next());
-        assertEquals("<HTML><HEAD><TITLE>Not yet supported</TITLE></HEAD><BODY>501 - Not yet supported</BODY></HTML>",
-                     verifierSocket.bodyAsString());
+        assertEquals(NOT_SUPPORTED_MESSAGE_BODY, verifierSocket.bodyAsString());
     }
 
 }
